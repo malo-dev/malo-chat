02652bd446ed333558c0434ca4fd8cff9e5c5c03
malo-dev <leadermushio377@gmail.com>
2022-11-20 14:15:20 +0200

    Adding client react part without redux

--- Backend/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Database = () => {
-	mongoose.connect('mongodb+srv://malochat:malochat37700@cluster0.dysk2py.mongodb.net/malochat') 
-	.then(() => console.log('database is connected successfully '))
-	.catch(err => console.log(err))
-}
-module.exports = Database
--- Backend/middlewares/Error.js
+++
@@ -1,9 +0,0 @@
-const HandleError = (err, req, res, next) => {
-	// refractor
-	const StatusError = res.statusCode === 200 ? 200 : res.statusCode;
-	res.status(StatusError)
-	res.json({
-		message : err.message,
-	})
-}
-module.exports = { HandleError };
