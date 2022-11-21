fbf7311a8fb2904be81fac6e60f8992b9ba6e025
malo-dev <leadermushio377@gmail.com>
2022-11-21 06:03:48 +0200

    Finalisation of both side part of Post and share images

--- server/index.js
+++ server/index.js
@@ -6,11 +6,14 @@ import dotenv from 'dotenv'
 import AuthRoute from './Routes/AuthRoutes.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
+import UploadRoute from './Routes/UploadRoute.js'
 const app = express()
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 dotenv.config()
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
 mongoose.connect(process.env.MONGO_DB_URL)
 	.then(() => {
 	app.listen(process.env.PORT,()=>console.log("Listening on port http://localhost:"+process.env.PORT))
@@ -21,3 +24,4 @@ mongoose.connect(process.env.MONGO_DB_URL)
 app.use('/auth', AuthRoute)
 app.use('/user', UserRoute)
 app.use('/posts', PostRoute)
+app.use('/upload', UploadRoute)
